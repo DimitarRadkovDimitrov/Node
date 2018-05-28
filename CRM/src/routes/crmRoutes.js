900fdabb5b14e0f45b9a5d5231a7f14a656eefb0
Dimitar Dimitrov A <dimitar.a.dimitrov@ericsson.com>
2018-05-28 15:22:39 -0400

    Schema

--- CRM/src/routes/crmRoutes.js
+++ CRM/src/routes/crmRoutes.js
@@ -1,8 +1,17 @@
 const routes = (app) => {
     app.route('/contact')
 
-    .get((req, res) =>
-    res.send('GET req successful'))
+    .get((req, res, next) => 
+    {
+        //Middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request from: ${req.method}`)
+        next();
+    }, (req, res, next) => 
+    {
+        res.send('GET req successful');
+    })
+    
 
     .post((req, res) =>
     res.send('POST req successful'));
